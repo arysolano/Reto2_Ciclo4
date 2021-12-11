70f60cda9cae55d313202eb1c411e657c6f98868
arysolano <ary.solano.mt@correo.usa.co>
2021-12-11 13:54:16 -0500

    first commit

---
+++ src/main/java/Reto2_Web/Reto2WebApplication.java
@@ -0,0 +1,18 @@
+package Reto2_Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Ary Solano
+ */
+
+@SpringBootApplication
+public class Reto2WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2WebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Reto2_Web/Reto2WebApplicationTests.java
@@ -0,0 +1,18 @@
+package Reto2_Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *
+ * @author Ary Solano
+ */
+
+@SpringBootTest
+class Reto2WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
